aa82f08670290b768e44547513bc2b6ae05e7eab
qqisee <1475906890@qq.com>
2023-09-10 11:36:30 +0800

    withtestnet

--- options/api_options.go
+++ options/api_options.go
@@ -5,6 +5,7 @@ type ApiOptions struct {
 	Secret     string
 	Passphrase string
 	ClientId   string
+	TestNet    bool
 }
 
 type ApiOption func(options *ApiOptions)
@@ -30,5 +31,10 @@ func WithPassphrase(passphrase string) ApiOption {
 func WithClientId(clientId string) ApiOption {
 	return func(options *ApiOptions) {
 		options.ClientId = clientId
+	}
+}
+func WithTestNet(testNet bool) ApiOption {
+	return func(options *ApiOptions) {
+		options.TestNet = testNet
 	}
 }
